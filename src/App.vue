a1672ce44b21d27d17ea37896a01f3e40eadd6c6
Santiago Villa <santiagovilla@MBP-de-Santi.lan>
2020-07-22 01:05:07 -0300

    + Navigation Guards

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,14 @@
         </ul>
       </nav>
     </header>
-    <main>
-      <router-view/>
-    </main>
+      <div class="container">
+        <aside class="aside">
+          <router-view name="sidebar"/>
+        </aside>
+        <main>
+          <router-view/>
+        </main>
+    </div>
   </div>
 </template>
 
@@ -47,15 +52,14 @@ body {
   margin-top: 60px;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -78,5 +82,16 @@ ul {
 }
 .router-link-active {
   color: white;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
 }
 </style>
--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -77,10 +77,21 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      /* eslint no-alert: 0 */
+      /* eslint no-restricted-globals: 0 */
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -113,6 +124,7 @@ export default {
         + robot.rightArm.cost
         + robot.base.cost;
       this.cart.push({ ...robot, cost });
+      this.addedToCart = true;
     },
   },
 };
